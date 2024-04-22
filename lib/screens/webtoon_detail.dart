d363201500b03cec2b1febfa97af9095b839ccc9
ohtaehyun <othdev95@gmail.com>
2024-04-22 20:16:24 +0900

    에피소드 버튼 추가

--- lib/screens/webtoon_detail.dart
+++ lib/screens/webtoon_detail.dart
@@ -42,70 +42,114 @@ class _DetailScreenState extends State<DetailScreen> {
           style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Hero(
-                tag: widget.id,
-                child: Container(
-                  width: 150,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                            blurRadius: 5,
-                            offset: const Offset(10, 10),
-                            color: Colors.black.withOpacity(0.5))
-                      ]),
-                  child: Image.network(
-                    widget.thumb,
-                    headers: const {'Referer': 'https://comic.naver.com'},
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Hero(
+                  tag: widget.id,
+                  child: Container(
+                    width: 150,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 5,
+                              offset: const Offset(10, 10),
+                              color: Colors.black.withOpacity(0.5))
+                        ]),
+                    child: Image.network(
+                      widget.thumb,
+                      headers: const {'Referer': 'https://comic.naver.com'},
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          FutureBuilder(
-            future: webtoon,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 50),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        snapshot.data!.about,
-                        style: const TextStyle(
-                          fontSize: 16,
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            FutureBuilder(
+              future: webtoon,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 33),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
-                        style: const TextStyle(
-                          fontSize: 12,
+                        const SizedBox(
+                          height: 8,
                         ),
-                      ),
-                    ],
-                  ),
-                );
-              }
-              return const Text('...');
-            },
-          )
-        ],
+                        Text(
+                          '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        FutureBuilder(
+                          future: episodes,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              return Column(
+                                children: [
+                                  for (var episode in snapshot.data!)
+                                    Container(
+                                      margin: const EdgeInsets.only(bottom: 5),
+                                      decoration: BoxDecoration(
+                                        color: Colors.green.shade300,
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 10, horizontal: 20),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              episode.title,
+                                              style: const TextStyle(
+                                                  color: Colors.white),
+                                            ),
+                                            const Icon(
+                                              Icons.chevron_right_rounded,
+                                              color: Colors.white,
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    )
+                                ],
+                              );
+                            }
+                            return Container();
+                          },
+                        )
+                      ],
+                    ),
+                  );
+                }
+                return const Text('...');
+              },
+            )
+          ],
+        ),
       ),
     );
   }
